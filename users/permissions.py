f96c7c4fef175073f3c829d514ff1c4a3652284f
DavidKovalov92 <kovalovdavid92@gmail.com>
2025-08-09 13:40:13 +0300

    permissions add

--- users/permissions.py
+++ users/permissions.py
@@ -1,11 +1,27 @@
 from rest_framework.permissions import BasePermission
-from .models import User
+from .models import Role
 
 class RoleHelper:
     @staticmethod
     def is_admin(user):
-        return user.role == UserRole.ADMIN
+        return user.role == Role.ADMIN
+    
+    @staticmethod
+    def is_manager(user):
+        return user.role == Role.MANAGER
+    
+    @staticmethod
+    def is_user(user):
+        return user.role == Role.USER
 
 class IsAdmin(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and RoleHelper.is_admin(request.user)
+
+class IsManager(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and RoleHelper.is_admin(request.user)
+
+class IsUser(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and RoleHelper.is_admin(request.user)
