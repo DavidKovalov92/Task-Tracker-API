e8ce961949837c78b52d58c8c2d3a652c5819881
DavidKovalov92 <kovalovdavid92@gmail.com>
2025-08-09 13:22:31 +0300

    dev -> user

--- users/models.py
+++ users/models.py
@@ -5,10 +5,10 @@ class CustomUser(AbstractUser):
     class Role(models.TextChoices):
         ADMIN = 'admin', 'Admin'
         MANAGER = 'manager', 'Manager'
-        DEVELOPER = 'developer', 'Developer'
+        USER = 'developer', 'Developer'
 
     role = models.TextChoices(
         max_length=20,
         choices=Role.choices,
-        default=Role.DEVELOPER,
+        default=Role.USER,
     )
