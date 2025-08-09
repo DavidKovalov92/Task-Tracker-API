47ffbfe37bd17d32799cb5747ef5c259fb5ceea7
DavidKovalov92 <kovalovdavid92@gmail.com>
2025-08-09 13:19:04 +0300

    add roles

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    class Role(models.TextChoices):
+        ADMIN = 'admin', 'Admin'
+        MANAGER = 'manager', 'Manager'
+        DEVELOPER = 'developer', 'Developer'
+
+    role = models.TextChoices(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.DEVELOPER,
+    )
