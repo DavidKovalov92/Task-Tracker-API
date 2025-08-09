b1d6f779c0574a697d426b49b7a1951e8a1edda6
DavidKovalov92 <kovalovdavid92@gmail.com>
2025-08-09 14:12:48 +0300

    task model add

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Status(models.TextChoices):
+    TODO = 'todo', 'ToDo'
+    IN_PROGRESS = 'in_progress', 'In Progress'
+    REVIEW = 'review', 'Review'
+    DONE = 'done', 'Done'
+    CANCELED = 'canceled', 'Canceled'
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=2000, null=True, blank=True)
+    status = models.CharField(
+        max_length=20,
+        choices=Status.choices,
+        default=Status.TODO
+    )
